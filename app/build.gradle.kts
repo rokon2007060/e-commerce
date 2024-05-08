e9d7bc20ef283038e061ccf5d22fbf9237c6e468
Mahi <ahnafraismahi@gmail.com>
2024-05-09 01:25:39 +0600

    data fetching and showing on mainactivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.firebase.firestore)
+    implementation("com.google.firebase:firebase-firestore:23.0.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/e_commerce_app/MainActivity.java
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.java
@@ -4,21 +4,50 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        txt=findViewById(R.id.prf);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Access individual documents/data here
+                            String fullName = document.getString("full_name");
+                            String email = document.getString("email");
+                            String phone = document.getString("phone");
+                            String address = document.getString("address");
+                            StringBuilder userData = new StringBuilder();
+                            userData.append("Full Name: ").append(fullName).append("\n");
+                            userData.append("Email: ").append(email).append("\n");
+                            userData.append("Phone: ").append(phone).append("\n");
+                            userData.append("Address: ").append(address);
 
+                            txt.setText(userData.toString());
+                            // Use retrieved data as needed
+                        }
+                    } else {
+                        // Handle errors
+                    }
+                });
 
     }
     public void logout(View view) {
