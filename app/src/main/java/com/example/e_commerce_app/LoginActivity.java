385b05f120272220d5a8ed647722e30ef56c620b
Mahi <ahnafraismahi@gmail.com>
2024-05-17 01:19:06 +0600

    resolved login problem(automatic app crash while logging)

--- app/src/main/java/com/example/e_commerce_app/LoginActivity.java
+++ app/src/main/java/com/example/e_commerce_app/LoginActivity.java
@@ -8,60 +8,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText emailTextView, passwordTextView;
-    private TextView forgetview;
-    private Button Btn;
-    private ProgressBar progressbar;
+    private Button loginButton;
+    private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
-    boolean valid = true;
-   FirebaseFirestore fstore;
+    private FirebaseFirestore fstore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
         mAuth = FirebaseAuth.getInstance();
-        fstore=FirebaseFirestore.getInstance();
-        // initialising all views through id defined above
+        fstore = FirebaseFirestore.getInstance();
+
         emailTextView = findViewById(R.id.email);
         passwordTextView = findViewById(R.id.password);
-        forgetview=findViewById(R.id.txtfpsw);
-        Btn = findViewById(R.id.login);
-        progressbar = findViewById(R.id.progressBar);
-        //checkField(emailTextView);
-        //checkField(passwordTextView);
+        loginButton = findViewById(R.id.login);
+        progressBar = findViewById(R.id.progressBar);
 
-        // Set on Click Listener on Sign-in button
-        Btn.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginUserAccount();
@@ -70,59 +51,50 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUserAccount() {
+        progressBar.setVisibility(View.VISIBLE);
 
-        // show the visibility of progress bar to show loading
-        progressbar.setVisibility(View.VISIBLE);
+        String email = emailTextView.getText().toString();
+        String password = passwordTextView.getText().toString();
 
-        // Take the value of two edit texts in Strings
-        String email, password;
-        email = emailTextView.getText().toString();
-        password = passwordTextView.getText().toString();
-
-        // validations for input email and password
         if (TextUtils.isEmpty(email)) {
-            Toast.makeText(getApplicationContext(),
-                            "Please enter email!!",
-                            Toast.LENGTH_LONG)
-                    .show();
+            Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(View.GONE);
             return;
         }
 
         if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(),
-                            "Please enter password!!",
-                            Toast.LENGTH_LONG)
-                    .show();
+            Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(View.GONE);
+            return;
+        }
+        if (TextUtils.isEmpty(password) && TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter password and email", Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(View.GONE);
             return;
         }
 
-        // signIn existing user
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            if (user != null) {
-                                // User is authenticated, proceed with your logic
-                                Toast.makeText(getApplicationContext(), "Login successful!!", Toast.LENGTH_LONG).show();
-                                checkAccessLevel(user.getUid());
-                                finish();
+                    public void onComplete(Task<AuthResult> task) {
+                        try {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if (user != null) {
+                                    Toast.makeText(getApplicationContext(), "Login successful!!", Toast.LENGTH_LONG).show();
+                                    checkAccessLevel(user.getUid());
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "User object is null", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.GONE);
+                                }
                             } else {
-                                // User object is null, handle accordingly
-                                Toast.makeText(getApplicationContext(), "User object is null", Toast.LENGTH_LONG).show();
+                                Toast.makeText(getApplicationContext(), "Login failed!!", Toast.LENGTH_LONG).show();
+                                progressBar.setVisibility(View.GONE);
                             }
-                        }
-
-                        else {
-
-                            // sign-in failed
-                            Toast.makeText(getApplicationContext(),
-                                            "Login failed!!",
-                                            Toast.LENGTH_LONG)
-                                    .show();
-                            // hide the progress bar
-                            progressbar.setVisibility(View.GONE);
+                        } catch (Exception e) {
+                            Log.e("LoginActivity", "Error in login", e);
+                            Toast.makeText(getApplicationContext(), "An error occurred, please try again later", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
                         }
                     }
                 });
@@ -133,45 +105,40 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void fpsw(View view) {
-        startActivity(new Intent(this,ForgotPasswordActivity.class));
+        startActivity(new Intent(this, ForgotPasswordActivity.class));
     }
+
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-        if (FirebaseAuth.getInstance().getCurrentUser()!=null){
-           DocumentReference df=FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
-           df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-               @Override
-               public void onSuccess(DocumentSnapshot documentSnapshot) {
-                   if (documentSnapshot.getString("isAdmin")!=null){
-                       startActivity(new Intent(getApplicationContext(), AdminActivity.class));
-                   }
-                   if (documentSnapshot.getString("isUser")!=null){
-                       startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                   }
-               }
-           }).addOnFailureListener(new OnFailureListener() {
-               @Override
-               public void onFailure(@NonNull Exception e) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                finish();
-               }
-           });
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            checkAccessLevel(currentUser.getUid());
         }
     }
-    private void  checkAccessLevel(String uid){
-        DocumentReference df=fstore.collection("users").document(uid);
-        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+
+    private void checkAccessLevel(String uid) {
+        DocumentReference df = fstore.collection("users").document(uid);
+        df.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                Log.d("Tag","onSuccess"+documentSnapshot.getData());
-                if (documentSnapshot.getString("isAdmin") != null) {
-                    startActivity(new Intent(LoginActivity.this, AdminActivity.class));
-                    finish();
-                } else if (documentSnapshot.getString("isUser") != null) {
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    finish();
+            public void onComplete(Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null) {
+                        if (document.getString("isAdmin") != null) {
+                            startActivity(new Intent(LoginActivity.this, AdminActivity.class));
+                            finish();
+                        } else if (document.getString("isUser") != null) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "User data not found", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "User data not found", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Failed to get user data", Toast.LENGTH_SHORT).show();
                 }
             }
         });
