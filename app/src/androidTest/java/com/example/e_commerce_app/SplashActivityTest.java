2a696fc2e8ef9f1959872b43569e666ebd14c01c
Mahi <ahnafraismahi@gmail.com>
2024-05-26 13:10:35 +0600

    ci testing

--- app/src/androidTest/java/com/example/e_commerce_app/SplashActivityTest.java
+++ app/src/androidTest/java/com/example/e_commerce_app/SplashActivityTest.java
@@ -1,59 +1,54 @@
 package com.example.e_commerce_app;
 
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.IdlingResource;
-import androidx.test.espresso.matcher.ViewMatchers;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.not;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 
 @RunWith(AndroidJUnit4.class)
 public class SplashActivityTest {
 
-    // Rule to launch SplashActivity
     @Rule
-    public ActivityScenarioRule<SplashActivity> activityScenarioRule = new ActivityScenarioRule<>(SplashActivity.class);
-
-    private IdlingResource idlingResource;
-
-    @Before
-    public void setUp() {
-        idlingResource = new SimpleIdlingResource();
-        IdlingRegistry.getInstance().register(idlingResource);
-    }
-
-    @After
-    public void tearDown() {
-        IdlingRegistry.getInstance().unregister(idlingResource);
-    }
+    public ActivityScenarioRule<SplashActivity> activityRule =
+            new ActivityScenarioRule<>(SplashActivity.class);
 
     @Test
-    public void testSplashScreenDisplayed() {
-        // Check if progress bar is displayed
+    public void splashScreen_isDisplayed() {
+        // Check that the progress bar is displayed
         onView(withId(R.id.progressBar)).check(matches(isDisplayed()));
-
-        // Notify that splash screen loading is finished
-        ((SimpleIdlingResource) idlingResource).setIdleState(true);
     }
 
-    @Test
-    public void testSplashScreenDismissed() {
-        // Wait for splash screen to be dismissed
-        ((SimpleIdlingResource) idlingResource).setIdleState(true);
-
-        // Check if progress bar is not displayed
-        onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())));
-    }
+//    @Test
+//    public void splashScreen_navigatesToLoginActivity() {
+//        // Initialize intents recording
+//        Intents.init();
+//
+//        // Wait for the splash delay duration + buffer time
+//        try {
+//            Thread.sleep(SplashActivity.SPLASH_DELAY + 1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // Check that the LoginActivity is started
+//        intended(hasComponent(LoginActivity.class.getName()));
+//
+//        // Release intents recording
+//        Intents.release();
+//}
 }
